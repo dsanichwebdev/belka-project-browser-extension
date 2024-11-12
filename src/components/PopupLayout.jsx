981c068f5eded66c4f3b3651bc8509f801b17604
dsanichwebdev <dsanichwebdev@gmail.com>
2024-11-12 17:22:58 +0300

    added unathorized avatar icon and authentication button

--- src/components/PopupLayout.jsx
+++ src/components/PopupLayout.jsx
@@ -7,12 +7,15 @@ const Container = styled(Flex, {
   justifyContent: 'space-between',
   padding: '5px',
   width: '300px',
+  flexDirection: 'column',
 });
 
 const RowFlex = styled(Flex, {
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '100%',
 });
 
 const StyledHeading = styled(Heading, {
@@ -22,19 +25,64 @@ const StyledHeading = styled(Heading, {
   fontWeight: 'medium',
 });
 
-const StyledAvatar = styled('img', {
+const StyledLogo = styled('img', {
   width: '40px',
   height: '40px',
   borderRadius: '50%',
   objectFit: 'cover',
+  display: 'flex',
 });
+
+const StyledUnathorizedAvatar = styled('img', {
+  width: '30px',
+  height: '30px',
+  borderRadius: '50%',
+  objectFit: 'cover',
+  display: 'flex',
+  background: '#f0f0f0',
+  padding: '5px'
+});
+
+const StyledLoginButton = styled(IconButton, {
+  backgroundColor: '#ffbf00',
+  color: 'white',
+  borderRadius: '8px',
+  padding: '10px 20px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  cursor: 'pointer',
+  fontWeight: 'bold',
+  marginTop: '15px',
+  transition: 'background-color 0.3s',
+  border: 'none',
+  width: '100%',
+
+  '&:hover': {
+    backgroundColor: '#e6a900',
+  },
+
+  '&:active': {
+    backgroundColor: '#cc9200',
+  }
+});
+
+const authenticate = () => {
+  console.log('authenticate!')
+}
 
 const PopupLayout = () => (
   <Container>
     <RowFlex>
-      <StyledAvatar src="icons/icon128.png" alt="Extension Icon" />
-      <StyledHeading size="2">BelkaScope</StyledHeading>
+      <Flex style={{ display: 'flex', alignItems: 'center' }}>
+        <StyledLogo src="icons/icon128.png" alt="Extension Icon" />
+        <StyledHeading size="2">BelkaScope</StyledHeading>
+      </Flex>
+      <StyledUnathorizedAvatar src="icons/unathorized-user-icon.png"/>
     </RowFlex>
+    <StyledLoginButton onClick={authenticate}>
+      Авторизоваться
+    </StyledLoginButton>
   </Container>
 );
 
