75d96296b4540faac313802ac536b8c3908c4dcf
dsanichwebdev <dsanichwebdev@gmail.com>
2024-11-19 18:27:28 +0300

    logic for inject component in product details page

--- public/content-scripts/injectComponent.js
+++ public/content-scripts/injectComponent.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ProductIconWithPanel from '@components/ProductIconWithPanel';
+import ProductDetailsWidget from '@components/ProductDetailsWidget';
 
 const insertComponent = () => {
   const observer = new MutationObserver((mutations) => {
     mutations.forEach(() => {
       const productCards = document.querySelectorAll('.product-card__wrapper');
+      const detailsWrapperRightSide = document.querySelector('.product-page__aside-sticky');
 
-      productCards.forEach((card) => {
-        if (!card.querySelector('.belka-scope-button-wrapper')) {
-          card.style.position = 'relative';
-          const wrapper = document.createElement('div');
-          wrapper.classList.add('belka-scope-button-wrapper');
-          wrapper.style.position = 'absolute';
-          card.appendChild(wrapper);
-          ReactDOM.render(<ProductIconWithPanel />, wrapper);
-        }
-      });
+      if (productCards.length) {
+        productCards.forEach((card) => {
+          if (!card.querySelector('.belka-scope-button-wrapper')) {
+            card.style.position = 'relative';
+            const wrapper = document.createElement('div');
+            wrapper.classList.add('belka-scope-button-wrapper');
+            wrapper.style.position = 'absolute';
+            card.appendChild(wrapper);
+            ReactDOM.render(<ProductIconWithPanel />, wrapper);
+          }
+        });
+      }
+
+      if (detailsWrapperRightSide && !detailsWrapperRightSide.querySelector('.belka-scope-widget-wrapper-right')) {
+        detailsWrapperRightSide.style.position = 'relative';
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('belka-scope-widget-wrapper-right');
+        detailsWrapperRightSide.appendChild(wrapper);
+        ReactDOM.render(<ProductDetailsWidget />, wrapper);
+      }
     });
   });
 
